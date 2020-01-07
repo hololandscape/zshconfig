ee082346059e4ebfa5a3a90e663fc6694d208d35
youngman <shengquan.bu@iquantex.com>
2020-01-07 10:05:11 +0800

    Add new features. 1. Add oh-my-bash. 2. Modify install script. 3. Update README.
    Signed-off-by: youngman <shengquan.bu@iquantex.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a theme package that provides terminal embellishment for mac users. With iterm2, you can quickly configure a simple but beautiful terminal interface.
 
-In fact, this theme package comes from [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh), which also contains other open source components. We just packaged it for installation. Thanks to the original authors of oh-my-zsh and other open source community project contributors. Give us such a good tool.
+In fact, this theme package comes from [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh) and [oh-my-bash](https://github.com/ohmybash/oh-my-bash.git), which also contains other open source components. We just packaged it for installation. Thanks to the original authors of `oh-my-zsh` / `oh-my-bash` and other open source community project contributors. Give us such a good tool.
 
 ![](example/example.gif)
 
@@ -28,6 +28,39 @@ In fact, this theme package comes from [oh-my-zsh](https://github.com/robbyrusse
    ```
 5. Open you iterm2, --> `Preferences` --> `Profiles` --> `Colors` --> click `Colors Presets` --> `Import` --> Select iterm2.itermcolors file. --> Choose your favorite theme color(I prefer atom) --> Then set as default.
 6. Navigate to the `text` tag,we need to set the font used by iterm2. setup font use `Hack Nerd Font`, finally, after setting up, re-open iterm2 and enjoy it.
+
+---
+
+# This theme pack can also be installed on Linux distributions.
+1. Install the necessary font files (!Required)
+   ```
+   # On Debian or Ubuntu based Linux distribution,
+   sudo apt-get install fonts-powerline
+
+   # On fedora (tested on 28) or redhat based Linux distribution
+   sudo dnf install powerline-fonts
+
+   # On other environments
+   git clone https://github.com/powerline/fonts.git --depth=1
+
+   cd fonts
+
+   ./install.sh
+
+   cd ..
+
+   rm -rf fonts
+   ```
+
+2. Clone this repo, and into folder.
+   ```
+   git clone https://github.com/G-Club/zshconfig.git
+   cd zshconfig
+   ```
+3. Install script.
+   ```
+   ./install.sh
+   ```
 
 ## For the terminal of VScode
 
--- install.sh
+++ install.sh
@@ -1,34 +1,61 @@
 #!/usr/bin/env bash
-if [ $SHELL != '/bin/zsh' ];then
-	echo "There is no zsh in your system, check it and try again."
-	echo "Or if you default shell is not zsh,use command to change: chsh -s /bin/zsh YouName"
-	exit 1
+
+function install_zsh(){
+    echo "Install oh-my-zsh..."
+    cp -R .oh-my-zsh ~/
+    if [ -f ~/.zshrc ];then
+        mv ~/.zshrc ~/.zshrc_bak
+        cp ./.zshrc ~/.zshrc
+        cp ./.zprofile ~/.zprofile
+    else
+        cp ./.zshrc ~/.zshrc
+        cp ./.zprofile ~/.zprofile
+    fi	
+    echo "Install oh-my-zsh complete!"
+}
+
+function install_bash(){
+    echo "Install oh-my-bash..."
+    cp -R .oh-my-bash ~/
+    if [ -f ~/.bashrc ];then
+        mv ~/.bashrc ~/.bashrc_bak
+        cp ./.bashrc ~/.bashrc
+        sed -i "s/root/$HOME/" ~/.bashrc
+    else
+        cp ./.bashrc ~/.bashrc
+        sed -i "s/root/$HOME/" ~/.bashrc
+    fi	
+    echo "Install oh-my-bash complete!"
+}
+
+function install_vim(){
+    echo "Install vim theme..."
+    cp -R .vim ~/
+    if [ -f ~/.vimrc ];then
+        mv ~/.vimrc ~/.vimrc_bak
+        cp ./.vimrc ~/.vimrc
+    else
+        cp ./.vimrc ~/.vimrc
+    fi
+    echo "Install vim thene complete!"
+}
+
+if [ $SHELL == '/bin/zsh' ];then
+    install_zsh
+    install_vim
+    # kubectl auto-completion
+    which kubectl >/dev/null
+    if [ $? -eq 0 ];then
+        echo 'source <(kubectl completion zsh)' >> ~/.zshrc
+    fi
+elif [ $SHELL == '/bin/bash' ];then
+    install_bash
+    install_vim
+    # kubectl auto-completion
+    which kubectl >/dev/null
+    if [ $? -eq 0 ];then
+        echo 'source <(kubectl completion bash)' >> ~/.bashrc
+    fi
 fi
 
-echo "Install oh-my-zsh..."
-cp -R .oh-my-zsh ~/
-if [ -f ~/.zshrc ];then
-	mv ~/.zshrc ~/.zshrc_bak
-	cp ./.zshrc ~/.zshrc
-	cp ./.zprofile ~/.zprofile
-else
-	cp ./.zshrc ~/.zshrc
-	cp ./.zprofile ~/.zprofile
-fi	
-echo "Install complete!"
 
-echo "Install vim theme..."
-cp -R .vim ~/
-if [ -f ~/.vimrc ];then
-	mv ~/.vimrc ~/.vimrc_bak
-	cp ./.vimrc ~/.vimrc
-else
-	cp ./.vimrc ~/.vimrc
-fi
-echo "Install complete!"
-
-# kubectl auto-completion
-which kubectl >/dev/null
-if [ $? -eq 0 ];then
-	echo 'source <(kubectl completion zsh)' >> ~/.zshrc
-fi
